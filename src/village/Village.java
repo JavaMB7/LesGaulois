3a7fdaadf53208b412ae546e43bf1e55d3031f2b
mathieu <barrieresmathieu@gmail.com>
2022-09-26 19:38:47 +0200

    Fin TP1 + Village init

---
+++ src/village/Village.java
@@ -0,0 +1,18 @@
+package village;
+
+public class Village {
+	private String nom;
+
+	/**
+	 * @param nom
+	 */
+	public Village(String nom) {
+		this.nom = nom;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+	
+	
+}
