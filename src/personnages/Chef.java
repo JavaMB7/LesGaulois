8e283b1214321b6f61668b1243e8bf190e5442da
mathieu <barrieresmathieu@gmail.com>
2022-09-26 19:03:51 +0200

    Fin 1er scenario

--- src/personnages/Chef.java
+++ src/personnages/Chef.java
@@ -1,13 +1,16 @@
 package personnages;
 
-public class Gaulois {
+public class Chef {
 	private String nom;
 	private int force;
 	private int effetPotion = 1;
-
-	public Gaulois(String nom, int force) {
+	private Village village;
+	
+	public Chef(String nom, int force, int effetPotion, Village village) {
 		this.nom = nom;
 		this.force = force;
+		this.effetPotion = effetPotion;
+		this.village = village;
 	}
 	
 	public String getNom() {
@@ -19,16 +22,12 @@ public class Gaulois {
 	}
 	
 	private String prendreParole() {
-		return "Le gaulois " + nom + " : ";
+		return "Le chef " + nom + " du village " + village.getNom() + " : ";
 	}
 	
 	public void frapper(Romain romain) {
-		System.out.println(nom + " envoie un grand coup dans la mâchoire de "+ romain.getNom());
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " +
+				romain.getNom());
 		romain.recevoirCoup(force / 3);
-	}
-	
-	@Override
-	public String toString() {
-		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
 	}
 }
