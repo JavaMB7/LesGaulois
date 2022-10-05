cf6d4ae0314a040c703618521a53a7872e13556e
mathieu <barrieresmathieu@gmail.com>
2022-10-05 14:53:07 +0200

    Classe Village

--- src/personnages/Chef.java
+++ src/personnages/Chef.java
@@ -5,13 +5,11 @@ import village.Village;
 public class Chef {
 	private String nom;
 	private int force;
-	private int effetPotion;
 	private Village village;
 	
-	public Chef(String nom, int force, int effetPotion, Village village) {
+	public Chef(String nom, int force, Village village) {
 		this.nom = nom;
 		this.force = force;
-		this.effetPotion = effetPotion;
 		this.village = village;
 	}
 	
--- src/village/Village.java
+++ src/village/Village.java
@@ -1,18 +1,63 @@
 package village;
 
+import java.util.Arrays;
+
+import personnages.Chef;
+import personnages.Gaulois;
+
 public class Village {
 	private String nom;
+	private Chef chef;
+	private Gaulois[] villageois;
+	private int nbVillageois = 0;
 
 	/**
 	 * @param nom
 	 */
-	public Village(String nom) {
+	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
+		this.villageois = new Gaulois[nbVillageoisMaximum];
+	}
+	
+	public void setChef(Chef chef) {
+		this.chef = chef;
 	}
 
 	public String getNom() {
 		return nom;
 	}
 	
+	public void ajouterHabitant(Gaulois gaulois) {
+		villageois[nbVillageois] = gaulois;
+		nbVillageois += 1;
+	}
 	
+	public Gaulois trouverHabitant(int numVillageois) {
+		return villageois[numVillageois];
+	}
+	
+	public void afficherVillageois() {
+		System.out.println("Dans le village du chef " + chef.getNom() + " vivent les légendaires gaulois : ");
+		for(int i=0; i<villageois.length; i++) {
+			if(villageois[i] != null) {
+				System.out.println("- " + villageois[i].getNom() + " ");
+			}
+		}
+	}
+	
+	public static void main(String []args) {
+		Village village = new Village("Village des Irréductibles", 30);
+		Chef abraracourcix = new Chef("Abraracourcix", 6, village);
+		Gaulois asterix = new Gaulois("Asterix", 8);
+		Gaulois obelix = new Gaulois("Obelix", 25);
+		//Gaulois gaulois = village.trouverHabitant(30);
+		//Ce message apparait car l'indice 30 n'est pas dans le tableau car ce dernier va de 0->29
+		village.setChef(abraracourcix);
+		village.ajouterHabitant(asterix);
+		village.ajouterHabitant(obelix);
+//		Gaulois gaulois = village.trouverHabitant(1);
+//		System.out.println(gaulois);
+		//Le gaulois astérix étant le premier villageois ajouté, il est présent à l'indice 0 du tableau
+		village.afficherVillageois();
+	}
 }
