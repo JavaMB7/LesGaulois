de79c643db64eb2a371738cfd71bc778b0e046e0
mathieu <barrieresmathieu@gmail.com>
2022-10-05 16:45:09 +0200

    les enumerations

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,9 +1,12 @@
 package personnages;
 
 public class Romain {
+	
 	private String nom;
 	private int force;
-
+	private Equipement[] equipements = {null, null};
+	private int nbEquipement = 0;
+	
 	public Romain(String nom, int force){
 		this.nom = nom;
 		this.force = force;
@@ -23,7 +26,7 @@ public class Romain {
 	}
 	
 	public void recevoirCoup(int forceCoup) {
-		int forceFin = force;
+		int forceDebut = force;
 		assert force > 0 : "La force est supérieure à 0";
 		
 		force -= forceCoup;
@@ -32,11 +35,50 @@ public class Romain {
 		} else {
 			parler("J'abandonne...");
 		}
-		assert forceFin > force : "La force à bien diminuer";
+		assert forceDebut > force : "La force à bien diminuer";
+	}
+	
+	public void sEquiper(Equipement equipement) {
+		switch (nbEquipement) {
+			case 0:
+				if(equipements[nbEquipement] == null && equipements[nbEquipement+1] == null) {
+					equipements[nbEquipement]= equipement;
+					nbEquipement ++;
+					sequipe(equipement);
+				} else if(equipements[nbEquipement].toString().equals(equipement.toString()) || equipements[nbEquipement+1].toString().equals(equipement.toString())  ) {
+					dejaEquiper(equipement);
+				}
+				break;
+			case 1 :
+				if(equipements[nbEquipement] == null && !equipements[nbEquipement-1].toString().equals(equipement.toString())) {
+					equipements[nbEquipement]= equipement;
+					nbEquipement ++;
+					sequipe(equipement);
+				} else if(equipements[nbEquipement-1].toString().equals(equipement.toString())) {
+					dejaEquiper(equipement);
+				}
+
+				break;
+			case 2 :
+				System.out.println("Le soldat " + getNom() + " est déjà bien équipé");
+		}
+		
+	}
+
+	private void dejaEquiper(Equipement equipement) {
+		System.out.println("Le soldat " + getNom() + " possède déjà un " + equipement.toString());
+	}
+
+	private void sequipe(Equipement equipement) {
+		System.out.println("Le soldat " + getNom() + " s'équipe d'un " + equipement.toString());
 	}
 	
 	public static void main(String[] args) throws Exception {
 		Romain minus = new Romain("Minus", 6);
 		minus.recevoirCoup(2);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
 	}
 }
